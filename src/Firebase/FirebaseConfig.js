88d8dce7ee1016fe1b5345e16d7e4d010d2c33fb
Constance Essuman <essumanconstance514@gmail.com>
2024-11-19 08:00:12 +0000

    Firebase

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -3,7 +3,13 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
+
+import { getAuth } from 'firebase/auth';
+import { addDoc, collection, getDocs, getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCd3_4Q7QFjbOMTPmaz4y1aznh2Shr4H9Y",
   authDomain: "plantea-1d582.firebaseapp.com",
@@ -13,5 +19,28 @@ const firebaseConfig = {
   appId: "1:697918269526:web:524353801b0acd89f633f7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export const addEquipment = async (equipmentData) => {
+  try {
+    const docRef = await addDoc(collection(db, "equipment"), equipmentData);
+    console.log("Equipment added with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding equipment: ", e);
+  }
+};
+
+export const getEquipment = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "equipment"));
+    const equipmentList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    return equipmentList;
+  } catch (e) {
+    console.error("Error fetching equipment: ", e);
+  }
+};
+
+export { auth, db, storage };
